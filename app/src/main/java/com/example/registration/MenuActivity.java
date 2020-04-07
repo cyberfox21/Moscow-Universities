fd1919a8433b42b8af26432ee0e030729cde6092
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2020-04-08 01:47:13 +0300

    Applied Search uhuuuuuuuuu :)

--- app/src/main/java/com/example/registration/MenuActivity.java
+++ app/src/main/java/com/example/registration/MenuActivity.java
@@ -35,13 +35,10 @@ public class MenuActivity extends AppCompatActivity{
 
     private EditText search_panel;
     private ImageButton searchBtn;
-    //private Boolean flag = false;
 
-    //private FirebaseRecyclerAdapter<Card> adapter;
     private FirebaseRecyclerAdapter<Card, CardViewHolder> firebaseRecyclerAdapter;
     private DatabaseReference universitiesDatabase;
     private Query firebaseSearchQuery;
-    //private ;
 
     private String title;
     private String descr;
@@ -50,9 +47,6 @@ public class MenuActivity extends AppCompatActivity{
     private String x;
     private String y;
 
-    private int p;
-    //private String textTosearch;
-
     private RecyclerView listOfCards;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +54,9 @@ public class MenuActivity extends AppCompatActivity{
         setContentView(R.layout.menu);
 
         universitiesDatabase = FirebaseDatabase.getInstance().getReference("Universities");
-        //firebaseSearchQuery = universitiesDatabase.orderByChild("title").startAt("").endAt("" + "\uf8ff");
 
         search_panel = findViewById(R.id.search_panel);
         searchBtn = findViewById(R.id.searchBtn);
-
-        //Intent toDescriptionActivity = new Intent(this, DescriptionActivity.class);
 
         listOfCards = findViewById(R.id.list_of_cards);
         listOfCards.setHasFixedSize(true);
@@ -83,28 +74,6 @@ public class MenuActivity extends AppCompatActivity{
                 firebaseUniversitySearch(searchText);
             }
         });
-
-//        listOfCards.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                title = ((TextView)(view.findViewById(R.id.card_title))).getText().toString();
-//                descr = ((TextView)(view.findViewById(R.id.card_inv_descr))).getText().toString();
-//                image = ((TextView)(view.findViewById(R.id.card_inv_image))).getText().toString();
-//                site = ((TextView)(view.findViewById(R.id.card_inv_site))).getText().toString();
-//                x = ((TextView)(view.findViewById(R.id.card_inv_x))).getText().toString();
-//                y = ((TextView)(view.findViewById(R.id.card_inv_y))).getText().toString();
-//
-//                toDescriptionActivity.putExtra("title", title);
-//                toDescriptionActivity.putExtra("descr", descr);
-//                toDescriptionActivity.putExtra("image", image);
-//                toDescriptionActivity.putExtra("site", site);
-//                toDescriptionActivity.putExtra("x", Double.parseDouble(x));
-//                toDescriptionActivity.putExtra("y", Double.parseDouble(y));
-//                startActivity(toDescriptionActivity);
-//            }
-//        });
-        //displayAllCards();
-
         firebaseUniversitySearch("");
     }
 
@@ -113,30 +82,21 @@ public class MenuActivity extends AppCompatActivity{
         firebaseSearchQuery = universitiesDatabase.orderByChild("title").startAt(searchText).endAt(searchText + "\uf8ff");
 
         firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Card, CardViewHolder>(
-
-                Card.class,
-                R.layout.card,
-                CardViewHolder.class,
-                firebaseSearchQuery
-
+                Card.class, R.layout.card, CardViewHolder.class, firebaseSearchQuery
         ) {
             @Override
             protected void populateViewHolder(CardViewHolder viewHolder, Card model, int position) {
-                p = position;
-                viewHolder.setDetails(getApplicationContext(), model.getTitle(), model.getTitle_descr(), model.getLogo(), model.getImage(), model.getDescr(),  model.getSite(), model.getX(), model.getY());
-
+                viewHolder.setDetails(getApplicationContext(), model.getTitle(), model.getTitle_descr(),
+                        model.getLogo(), model.getImage(), model.getDescr(),  model.getSite(), model.getX(), model.getY());
             }
         };
 
         listOfCards.setAdapter(firebaseRecyclerAdapter);
         listOfCards.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
             GestureDetector gestureDetector = new GestureDetector(MenuActivity.this, new GestureDetector.SimpleOnGestureListener() {
-
                 @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
-
                     return true;
                 }
-
             });
             @Override
             public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
@@ -160,106 +120,26 @@ public class MenuActivity extends AppCompatActivity{
                 }
                 return false;
             }
-
             @Override
             public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-
-
             }
-
             @Override
             public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
 
             }
         });
     }
-
     public void enableSearchPanel(View v) {
         v.setFocusable(true);
         v.setEnabled(true);
         v.setFocusableInTouchMode(true);
         v.setClickable(true);
     }
-
-//    private void displayAllCards() {
-////        //ListView listOfCards = findViewById(R.id.list_of_cards);
-////        //ArrayList<Card> listItems = new ArrayList<>();
-////
-////        adapter = new FirebaseListAdapter<Card>(this, Card.class, R.layout.card,
-////                FirebaseDatabase.getInstance().getReference().child("Universities")) {
-////            @Override
-////            protected void populateView(View v, Card model, int position) {
-////                TextView vis_title, vis_descr;
-////                ImageView vis_image;
-////                    vis_title = v.findViewById(R.id.card_title);
-////                    vis_descr = v.findViewById(R.id.card_descr);
-////                    vis_image = v.findViewById(R.id.card_image);
-////
-////                    vis_title.setText(model.getTitle());
-////                    vis_descr.setText(model.getTitle_descr());
-////                    Picasso.with(MenuActivity.this).load(model.getLogo()).into(vis_image);
-////
-////                    TextView inv_descr = v.findViewById(R.id.card_inv_descr);
-////                    TextView inv_image = v.findViewById(R.id.card_inv_image);
-////                    TextView inv_site = v.findViewById(R.id.card_inv_site);
-////                    TextView inv_x = v.findViewById(R.id.card_inv_x);
-////                    TextView inv_y = v.findViewById(R.id.card_inv_y);
-////
-////                    inv_image.setText(model.getImage());
-////                    inv_descr.setText(model.getDescr());
-////                    inv_site.setText(model.getSite());
-////                    inv_x.setText((model.getX()).toString());
-////                    inv_y.setText((model.getY()).toString());
-////            }
-////        };
-////        listOfCards.setAdapter(adapter);
-////    }
-//    private OnItemClickListener myListener;
-//
-//    public interface OnItemClickListener {
-//        void onItemClick(int position);
-//    }
-//
-//    public void setOnItemClickListener(OnItemClickListener listener){
-//        myListener = listener;
-//    }
-
     public static class CardViewHolder extends RecyclerView.ViewHolder {
-
         View view;
-
-
         public CardViewHolder(View itemView) {
-            //, final OnItemClickListener listener)
             super(itemView);
             view = itemView;
-//            view.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-////                    if (listener != null){
-////                        int position = getAdapterPosition();
-////                        if(position != )
-////                    }
-//                String title = ((TextView)(view.findViewById(R.id.card_title))).getText().toString();
-//                String descr = ((TextView)(view.findViewById(R.id.card_inv_descr))).getText().toString();
-//                String image = ((TextView)(view.findViewById(R.id.card_inv_image))).getText().toString();
-//                String site = ((TextView)(view.findViewById(R.id.card_inv_site))).getText().toString();
-//                String x = ((TextView)(view.findViewById(R.id.card_inv_x))).getText().toString();
-//                String y = ((TextView)(view.findViewById(R.id.card_inv_y))).getText().toString();
-//
-//                Intent toDescriptionActivity = new Intent(MenuActivity.this, DescriptionActivity.class);
-//
-//                toDescriptionActivity.putExtra("title", title);
-//                toDescriptionActivity.putExtra("descr", descr);
-//                toDescriptionActivity.putExtra("image", image);
-//                toDescriptionActivity.putExtra("site", site);
-//                toDescriptionActivity.putExtra("x", Double.parseDouble(x));
-//                toDescriptionActivity.putExtra("y", Double.parseDouble(y));
-//                startActivity(toDescriptionActivity);
-//
-//                }
-//            });
-
         }
         public void setDetails(Context ctx, String card_title, String card_text, String card_image, String image, String descr, String card_site, Double card_x, Double card_y){
 
