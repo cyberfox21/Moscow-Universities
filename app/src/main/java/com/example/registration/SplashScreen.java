06861f4fb112325b5e780c2c54f9bb50bdca7e6f
Tatyana Shkolnik <shkolniktatyana21@gmail.com>
2020-04-12 02:18:29 +0300

    Design of SplashActivity was changed.

--- app/src/main/java/com/example/registration/SplashScreen.java
+++ app/src/main/java/com/example/registration/SplashScreen.java
@@ -5,20 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import static java.lang.Thread.sleep;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         try {
-            Thread.sleep(1000); //Приостанавливает поток на 1 секунду
+            sleep(1000);
         } catch (Exception e) {
 
         }
 
-            Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
+            setContentView(R.layout.activity_splash_screen);
+            Thread welcomeThread = new Thread() {
 
-        finish();
-    }
+                @Override
+                public void run() {
+                    try {
+                        super.run();
+                        sleep(1500);
+                    } catch (Exception e) {
+
+                    } finally {
+                        Intent i = new Intent(SplashScreen.this,
+                                MenuActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }
+            };
+            welcomeThread.start();
+        }
 }
